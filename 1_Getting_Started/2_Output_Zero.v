bbe1bf213798637f9a0eb344f477ef127885c142
matbi86 <joohankim86@gmail.com>
2024-10-16 19:41:10 +0900

    updated minor error

--- 1_Getting_Started/2_Output_Zero.v
+++ 1_Getting_Started/2_Output_Zero.v
@@ -1,5 +1,5 @@
 module top_module (
     output zero  // 출력 신호 선언
 );
-    assign zero = 0;  // 항상 0을 출력
+    assign zero = 1'b0;  // 항상 0을 출력
 endmodule
--- 2_Verilog_Language/1_Basics/8_XNOR_gate.v
+++ 2_Verilog_Language/1_Basics/8_XNOR_gate.v
@@ -1,3 +1,4 @@
 module top_module( input a, input b, output out );
     assign out = ~(a ^ b);
+    //assign out = a ~^ b; // same results
 endmodule
